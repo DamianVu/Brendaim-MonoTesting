7f12acb869080c8e1b4b722ae5eb0cf167b0a216
Damian <damian@vuhoo.org>
2018-06-06 21:39:02 -0500

    Renamed everything to Brendaim

--- Game1/Game1/Brendaim.cs
+++ Game1/Game1/Brendaim.cs
@@ -7,12 +7,12 @@ namespace Game1
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class Brendaim : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public Brendaim()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
