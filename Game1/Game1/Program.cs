dce80000cb9c620c93e7a95446bdb258bf662d12
Damian <damian@vuhoo.org>
2018-06-06 21:44:51 -0500

    Ayy lmao

--- Game1/Game1/Program.cs
+++ Game1/Game1/Program.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 
-namespace Game1
+namespace Brendaim
 {
 #if WINDOWS || LINUX
-    /// <summary>
-    /// The main class.
-    /// </summary>
+
     public static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+
         [STAThread]
         static void Main()
         {
