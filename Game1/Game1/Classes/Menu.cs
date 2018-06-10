b862422b4b9a4179d6e16bbfc216915b1c887f1b
Damian <damian@vuhoo.org>
2018-06-10 14:48:00 -0500

    Beginning scaffolding for menus

---
+++ Game1/Game1/Classes/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Brendaim.Menu
+{
+    public class Menu
+    {
+
+        public Boolean Visible { get; set; }
+
+        // Initialize a blank menu
+        public Menu()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+    }
+}
+
+
